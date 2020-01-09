07d77f567be61a7df974d872918345c30d80127a
WuChang <w1940285220@gmail.com>
2020-01-09 11:25:23 +0800

    第一次提交。

---
+++ src/main/java/com/nightmare/wu/WuApplication.java
@@ -0,0 +1,13 @@
+package com.nightmare.wu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuApplication.class, args);
+    }
+
+}
